091fb4140c121149a9e9fa62791ffc371350d6e6
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-09-22 23:12:40 -0500

    finished addtransaction componet

---
+++ src/AddTransaction.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {ref, defineEmits} from 'vue'
+const text = ref('')
+const amount = ref('')
+
+const emit = defineEmits([
+    'transactionSubmitted'
+
+])
+
+const onSubmit = () =>{
+    const transactionData = {
+        text: text.value,
+        amount: paraseFloat(amount.value),
+    }
+
+    emit('transactionSubmitted', transactionData)
+
+    text.value = ''
+    amount.value = ''
+
+}
+</script>
+
+
+<template>
+<h3>Add a new Transaction</h3>
+<form id="form" @submit.prevent="onSubmit">
+    <div class="form-control">
+        <label for="text">Enter Transaction</label>
+        <input type="text" id="text" v-model="text" placeholder="Enter transaction...">
+    </div>
+    <div class="form-control">
+        <label for="amount">Enter Transaction Amount</label>
+        <input type="text" id="amount" v-model="amount" placeholder="Enter Negative Value for Expenses...">
+    </div>
+    <button class="btn">Add Transaction</button>
+</form>
+    
+</template>
--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Header from './components/Head.vue';
 import Balance from './components/Balance.vue';
 import {ref,computed} from 'vue'
 import IncomeExpenses from './components/IncomeExpenses.vue';
+import AddTransaction from './AddTransaction.vue';
 
 const transactions = ref([
     {id: 1, text:'Paycheck', amount: 700.00},
@@ -39,5 +40,6 @@ const moneyOut = computed(()=>{
 <div class="container">
     <Balance :total="sum"></Balance>
     <IncomeExpenses :income="moneyIn" :expense="moneyOut"> </IncomeExpenses>
+    <AddTransaction></AddTransaction>
 </div>
 </template>
